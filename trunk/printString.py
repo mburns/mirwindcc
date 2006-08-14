34d6cbbebe59295d7db2c76ccdda11c8f359ff20
jairwin <jairwin@1b6ac8b3-f31a-0410-b181-d3a4b060f72f>
2006-08-14 17:09:08 +0000

    Expanded the hello world program to be modular. Python is amazing

---
+++ trunk/printString.py
@@ -0,0 +1,10 @@
+def printString(phrase):
+	""" Prints the string sent in.
+
+
+	Created to test out modules functionality"""
+	print phrase
+
+if __name__ == "__main__":
+	print "You are running this incorrectly."
+
